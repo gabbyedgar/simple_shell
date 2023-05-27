5ab41ddf36bb75916ad84f2d6775400d59c00ef9
Ochiba Gabriel <122891682+gabbyedgar@users.noreply.github.com>
2023-05-27 18:24:37 +0100

    Update str_aux.c

--- str_aux.c
+++ str_aux.c
@@ -1,30 +1,31 @@
 #include "main.h"
 
 /**
- * rev_string - reverses a string.
- * @s: input string.
- * Return: no return.
+ * @info: Structure containing potential arguments. Used to maintain
+ *         constant function prototype.
+ *
+ * Return: Always 0
  */
 void rev_string(char *s)
 {
 	int count = 0, i, j;
 	char *str, temp;
 
-	while (count >= 0)
-	{
-		if (s[count] == '\0')
-			break;
-		count++;
-	}
-	str = s;
-
-	for (i = 0; i < (count - 1); i++)
-	{
-		for (j = i + 1; j > 0; j--)
+	for (j = i + 1; j > 0; j--)
 		{
 			temp = *(str + j);
 			*(str + j) = *(str + (j - 1));
 			*(str + (j - 1)) = temp;
 		}
+	str = s;
+
+	for (i = 0; i < (count - 1); i++)
+	{
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}	
 	}
 }
