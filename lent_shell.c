71de281a9b2ea43bc60028176fc5004a0bf031ba
Ochiba Gabriel <122891682+gabbyedgar@users.noreply.github.com>
2023-05-27 18:06:14 +0100

    Update lent_shell.c

--- lent_shell.c
+++ lent_shell.c
@@ -1,29 +1,6 @@
 #include "main.h"
 
 /**
- * _strcat - concatenate two strings
- * @dest: char pointer the dest of the copied str
- * @src: const char pointer the source of str
- * Return: the dest
- */
-char *_strcat(char *dest, const char *src)
-{
-	int i;
-	int j;
-
-	for (i = 0; dest[i] != '\0'; i++)
-		;
-
-	for (j = 0; src[j] != '\0'; j++)
-	{
-		dest[i] = src[j];
-		i++;
-	}
-
-	dest[i] = '\0';
-	return (dest);
-}
-/**
  * *_strcpy - Copies the string pointed to by src.
  * @dest: Type char pointer the dest of the copied str
  * @src: Type char pointer the source of str
@@ -34,50 +11,53 @@ char *_strcpy(char *dest, char *src)
 
 	size_t a;
 
-	for (a = 0; src[a] != '\0'; a++)
-	{
-		dest[a] = src[a];
-	}
-	dest[a] = '\0';
-
-	return (dest);
-}
-/**
- * _strcmp - Function that compares two strings.
- * @s1: type str compared
- * @s2: type str compared
- * Return: Always 0.
+	f/**
+ * free_sep_list - frees a sep_list
+ * @head: head of the linked list.
+ * Return: no return.
  */
-int _strcmp(char *s1, char *s2)
+void free_sep_list(sep_list **head)
 {
-	int i;
+	if (head == NULL || *head == NULL)
+		return;
 
+	sep_list *current = *head;
+	sep_list *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+
+	*head = NULL;
+}
+
+/**
+ * add_line_node_end - adds a command line at the end
+ * of a line_list.
+ * @head: head of the linked list.
+ * @line: command line.
+ * Return: address of the head.
+ */
+line_list *add_line_node_end(line_list **head, char *line)
+{
+	line_list *new = malloc(sizeof(line_list));
+	if (new == NULL)
+		return NULL;
+
+	
+	return *head;
 	for (i = 0; s1[i] == s2[i] && s1[i]; i++)
 		;
 
 	if (s1[i] > s2[i])
 		return (1);
-	if (s1[i] < s2[i])
-		return (-1);
-	return (0);
-}
-/**
- * _strchr - locates a character in a string,
- * @s: string.
- * @c: character.
- * Return: the pointer to the first occurrence of the character c.
- */
-char *_strchr(char *s, char c)
-{
-	unsigned int i = 0;
 
-	for (; *(s + i) != '\0'; i++)
-		if (*(s + i) == c)
-			return (s + i);
-	if (*(s + i) == c)
-		return (s + i);
-	return ('\0');
+	return (dest);
 }
+
 /**
  * _strspn - gets the length of a prefix substring.
  * @s: initial segment.
@@ -86,21 +66,18 @@ char *_strchr(char *s, char c)
  */
 int _strspn(char *s, char *accept)
 {
-	int i, j, bool;
+	char *dir = datash->args[1];
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	if (is_directory_change(dir))
+		return 1;
+
+	if (is_home_change(dir))
 	{
-		bool = 1;
-		for (j = 0; *(accept + j) != '\0'; j++)
-		{
-			if (*(s + i) == *(accept + j))
-			{
-				bool = 0;
-				break;
-			}
-		}
-		if (bool == 1)
-			break;
+		cd_to_home(datash);
+		return 1;
 	}
-	return (i);
+
+	cd_to(datash);
+
+	return 1;
 }
