55e397d8cf306f6bbe524e7d7d16dcedbf26ebb7
Ochiba Gabriel <122891682+gabbyedgar@users.noreply.github.com>
2023-05-27 18:19:54 +0100

    Update shell_lists2.c

--- shell_lists2.c
+++ shell_lists2.c
@@ -1,43 +1,4 @@
 #include "main.h"
-
-/**
- * add_rvar_node - adds a variable at the end
- * of a r_var list.
- * @head: head of the linked list.
- * @lvar: length of the variable.
- * @val: value of the variable.
- * @lval: length of the value.
- * Return: address of the head.
- */
-r_var *add_rvar_node(r_var **head, int lvar, char *val, int lval)
-{
-	r_var *new, *temp;
-
-	new = malloc(sizeof(r_var));
-	if (new == NULL)
-		return (NULL);
-
-	new->len_var = lvar;
-	new->val = val;
-	new->len_val = lval;
-
-	new->next = NULL;
-	temp = *head;
-
-	if (temp == NULL)
-	{
-		*head = new;
-	}
-	else
-	{
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new;
-	}
-
-	return (*head);
-}
-
 /**
  * free_rvar_list - frees a r_var list
  * @head: head of the linked list.
@@ -52,6 +13,22 @@ void free_rvar_list(r_var **head)
 	{
 		curr = *head;
 		while ((temp = curr) != NULL)
+			char *dir = datash->args[1];
+
+	if (is_directory_change(dir))
+		return 1;
+
+	if (is_home_change(dir))
+	{
+		cd_to_home(datash);
+		return 1;
+	}
+
+	cd_to(datash);
+
+	return 1;
+}
+
 		{
 			curr = curr->next;
 			free(temp);
